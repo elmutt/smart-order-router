b38921fe341893efd6ff268339248d905164734c
Sean Luther <elmutt@gmail.com>
2022-12-28 21:30:02 -0700

    pass extra pool data

--- src/providers/uri-subgraph-provider.ts
+++ src/providers/uri-subgraph-provider.ts
@@ -1,12 +1,12 @@
-import retry from 'async-retry';
-import Timeout from 'await-timeout';
-import axios from 'axios';
+import retry from 'async-retry'
+import Timeout from 'await-timeout'
+import axios from 'axios'
 
-import { ChainId } from '../util/chains';
-import { log } from '../util/log';
+import { ChainId } from '../util/chains'
+import { log } from '../util/log'
 
-import { V2SubgraphPool } from './v2/subgraph-provider';
-import { V3SubgraphPool } from './v3/subgraph-provider';
+import { V2SubgraphPool } from './v2/subgraph-provider'
+import { V3SubgraphPool } from './v3/subgraph-provider'
 
 /**
  * Gets subgraph pools from a URI. The URI shoudl contain a JSON
@@ -20,58 +20,59 @@ import { V3SubgraphPool } from './v3/subgraph-provider';
 export class URISubgraphProvider<
   TSubgraphPool extends V2SubgraphPool | V3SubgraphPool
 > {
-  constructor(
+  constructor (
     private chainId: ChainId,
     private uri: string,
     private timeout = 6000,
-    private retries = 2
+    private retries = 2,
+    private extraPoolData = []
   ) {}
 
-  public async getPools(): Promise<TSubgraphPool[]> {
+  public async getPools (): Promise<TSubgraphPool[]> {
     log.info(
       { uri: this.uri },
       `About to get subgraph pools from URI ${this.uri}`
-    );
+    )
 
-    let allPools: TSubgraphPool[] = [];
+    let allPools: TSubgraphPool[] = []
 
     await retry(
       async () => {
-        const timeout = new Timeout();
+        const timeout = new Timeout()
         const timerPromise = timeout.set(this.timeout).then(() => {
           throw new Error(
             `Timed out getting pools from subgraph: ${this.timeout}`
-          );
-        });
+          )
+        })
 
-        let response;
+        let response
 
         /* eslint-disable no-useless-catch */
         try {
-          response = await Promise.race([axios.get(this.uri), timerPromise]);
+          response = await Promise.race([axios.get(this.uri), timerPromise])
         } catch (err) {
-          throw err;
+          throw err
         } finally {
-          timeout.clear();
+          timeout.clear()
         }
         /* eslint-enable no-useless-catch */
 
-        const { data: poolsBuffer, status } = response;
+        const { data: poolsBuffer, status } = response
 
         if (status != 200) {
-          log.error({ response }, `Unabled to get pools from ${this.uri}.`);
+          log.error({ response }, `Unabled to get pools from ${this.uri}.`)
 
-          throw new Error(`Unable to get pools from ${this.uri}`);
+          throw new Error(`Unable to get pools from ${this.uri}`)
         }
 
-        const pools = poolsBuffer as TSubgraphPool[];
+        const pools = poolsBuffer.concat(this.extraPoolData) as TSubgraphPool[]
 
         log.info(
           { uri: this.uri, chain: this.chainId },
           `Got subgraph pools from uri. Num: ${pools.length}`
-        );
+        )
 
-        allPools = pools;
+        allPools = pools
       },
       {
         retries: this.retries,
@@ -79,11 +80,11 @@ export class URISubgraphProvider<
           log.info(
             { err },
             `Failed to get pools from uri ${this.uri}. Retry attempt: ${retry}`
-          );
-        },
+          )
+        }
       }
-    );
+    )
 
-    return allPools;
+    return allPools
   }
 }
